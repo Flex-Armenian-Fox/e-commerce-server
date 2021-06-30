7116480697030e086c7ba34b0b035acd2d0b2f97
budigk <budigk@gmail.com>
2021-06-30 19:03:54 +0700

    Setting config and port

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require("dotenv").config();
+if(process.env.NODE_ENV != "production") require("dotenv").config();
 
 const express = require('express');
 const errHandler = require('./middlewares/errHandler.js');
 const cors = require('cors')
 const app = express();
-const PORT = 3000;
 const routes = require('./routes');
 
 app.use(cors());

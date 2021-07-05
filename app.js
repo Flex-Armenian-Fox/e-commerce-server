b6d3edf357fb1ba658046754aa0cf1823e048b1f
Karen <karen@KD-MM.local>
2021-07-05 22:00:50 +0700

    05 Jul - 22.00

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const route = require('./routes/route-main.js')
 const errorHandler = require('./helpers/errorHandler.js')
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 

dd8c29ee2de5b00f696be83b8bf3393482524c28
unknown <edgar.adirga@gmail.com>
2021-07-08 16:09:11 +0700

    server

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken")
 
-const SECRET_KEY = 'rahasia'
+const SECRET_KEY = process.env.SECRET_KEY
 const jwtEncrypt = (payload) => jwt.sign(payload, SECRET_KEY)
 
 const jwtDecrypt = (token) => jwt.verify(token, SECRET_KEY)
